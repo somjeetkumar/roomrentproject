c2fa37e5bfbb68ac5704a257dd68f5fbb0e8e192
somjeet <somjeetkumar30@gmail.com>
2026-06-11 07:50:10 +0530

    git add .

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import cloudinary
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +85,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
         'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
+
+
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         os.getenv("DATABASE_URL")
+#     )
+# }
 
 
 # Password validation
